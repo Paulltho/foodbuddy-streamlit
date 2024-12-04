7a00b188b52554e5e441c0a4e9f6a4143f3c8290
Paul Thomas <pthomas.2019@gmail.com>
2024-12-04 22:31:56 +0100

    newer

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from homepage import homepage
 from nutrition_form import nutrition_form
 from meal_analysis import meal_analysis
 
+
 # Access the variable from st.secrets
 SERVICE_URL = st.secrets["general"]["SERVICE_URL"]
 
-# Set up session state
+# Set up session state test
 if "page" not in st.session_state:
     st.session_state["page"] = "homepage"
+
 
 # Navigation logic
 if st.session_state["page"] == "homepage":
