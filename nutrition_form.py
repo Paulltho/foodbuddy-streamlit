d337e4a0f8259514b647abaae5cb30aff0b3134f
Paulltho <pthomas.2019@gmail.com>
2024-12-05 11:52:12 +0000

    new

--- nutrition_form.py
+++ nutrition_form.py
@@ -92,29 +92,3 @@ def nutrition_form():
 
         st.button("Scan my plate!", on_click=go_to_scan)
 
-
-st.markdown(
-    """
-    <style>
-    div.stButton {
-        display: flex;
-        justify-content: center; /* Centers the button horizontally */
-        margin-top: 10px; /* Adds spacing above the button */
-    }
-    div.stButton > button {
-        font-size: 20px; /* Makes the button text larger */
-        font-weight: bold; /* Makes the button text bold */
-        padding: 10px 20px; /* Adds padding around the text */
-        background-color: #FF5733; /* Sets the button background color to red */
-        color: white; /* Sets the text color to white */
-        border: none; /* Removes the button border */
-        border-radius: 8px; /* Adds rounded corners */
-        cursor: pointer; /* Changes the cursor to a pointer on hover */
-    }
-    div.stButton > button:hover {
-        background-color: #E74C3C; /* Darkens the red color on hover */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
