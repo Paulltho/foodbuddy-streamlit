99c5775408aef2b4c92ecf6e57c0ec1aa5b4adfc
Paul Thomas <pthomas.2019@gmail.com>
2024-12-05 11:39:07 +0100

    test

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import streamlit as st
+import time
+from loading import loading
 from homepage import homepage
 from nutrition_form import nutrition_form
 from meal_analysis import meal_analysis
 
-
-# Access the variable from st.secrets
-SERVICE_URL = st.secrets["general"]["SERVICE_URL"]
-
 # Set up session state test
 if "page" not in st.session_state:
-    st.session_state["page"] = "homepage"
-
+    st.session_state["page"] = "loading"
 
 # Navigation logic
+if st.session_state["page"] == "loading":
+    loading()
+    time.sleep(1)
+    st.session_state["page"] = "homepage"
 if st.session_state["page"] == "homepage":
     homepage()
 elif st.session_state["page"] == "nutrition_form":
---
+++ loading.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+# Function to display the loading page
+def loading():
+    st.image("logo.png", caption="")
