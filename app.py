a888a7b6c68f5acecd87c289d377b4dd8ff66ca0
Paul Thomas <pthomas.2019@gmail.com>
2024-12-05 11:45:03 +0100

    loading time

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from homepage import homepage
 from nutrition_form import nutrition_form
 from meal_analysis import meal_analysis
 
+def go_to_homepage():
+    st.session_state["page"] = "homepage"
+
 # Set up session state test
 if "page" not in st.session_state:
     st.session_state["page"] = "loading"
@@ -12,8 +15,8 @@ if "page" not in st.session_state:
 # Navigation logic
 if st.session_state["page"] == "loading":
     loading()
-    time.sleep(1)
-    st.session_state["page"] = "homepage"
+    time.sleep(2)
+    go_to_homepage()
 if st.session_state["page"] == "homepage":
     homepage()
 elif st.session_state["page"] == "nutrition_form":
