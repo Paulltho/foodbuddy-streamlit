b8c2d93bbdd7d3d8c55e9e52b6007234951e0f6e
Paul Thomas <pthomas.2019@gmail.com>
2024-12-05 11:46:42 +0100

    push html

--- loading.py
+++ loading.py
@@ -2,5 +2,31 @@ import streamlit as st
 
 # Function to display the loading page
 def loading():
-    st.image("logo.png", caption="")
-    st.title("FoodBuddy™")
+    st.markdown(
+        """
+        <style>
+        .centered-container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            text-align: center;
+        }
+        .logo {
+            width: 150px;
+            height: auto;
+        }
+        .title {
+            font-size: 2.5em;
+            color: #333;
+            margin-top: 20px;
+        }
+        </style>
+        <div class="centered-container">
+            <img class="logo" src="logo.png" alt="Logo">
+            <div class="title">FoodBuddy™</div>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
